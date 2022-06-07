b69b5fe18df452e785ba29d894c0934a0ddc355c
qkrgkdud3010 <qkrgkdud3010@naver.com>
2022-06-07 15:37:06 +0900

    테스트
    온도 습도 js 추가~

--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,28 @@
 
-document.getElementById("btn1").addEventListener('click',getLocation);
-
-function getLocation() {
-    if (navigator.geolocation) { // GPS를 지원하면
-      navigator.geolocation.getCurrentPosition(function(position) {
-        alert(position.coords.latitude + ' ' + position.coords.longitude);
-      }, function(error) {
-        console.error(error);
-      }, {
-        enableHighAccuracy: false,
-        maximumAge: 0,
-        timeout: Infinity
-      });
-    } else {
-      alert('GPS를 지원하지 않습니다');
-    }
-  }
+const API_KEY = 'f3e5f3bd0ed6a160508612f2a643403a';
  
+function onGeoOk(position) {
+   const latitude = position.coords.latitude;
+   const longitude = position.coords.longitude;
+ 
+   // console.log(`You live in ${latitude} and ${longitude}`);
+ 
+   fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`,
+   )
+      .then((response) => response.json())
+      .then((data) =>{
+        const tempcontainer = document.querySelector("#weather span:first-child")
+        const humcontainer = document.querySelector("#weather span:last-child")
+        humcontainer.innerText = data.main.humidity;
+        tempcontainer.innerText = data.main.temp;
+
+
+      }); 
+}
+ 
+function onGeoError() {
+   alert("Can't find you. No weather for you.");
+}
+ 
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
